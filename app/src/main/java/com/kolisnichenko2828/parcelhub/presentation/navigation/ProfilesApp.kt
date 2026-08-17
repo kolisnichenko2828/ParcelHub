3ef9963d31551b2dbb1a3f800fec6711bc0e1251
kolisnichenko2828 <kolisnichenko2828@gmail.com>
2026-08-17 15:46:59 +0300

    update: center content

--- app/src/main/java/com/kolisnichenko2828/parcelhub/presentation/navigation/ProfilesApp.kt
+++ app/src/main/java/com/kolisnichenko2828/parcelhub/presentation/navigation/ProfilesApp.kt
@@ -6,12 +6,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun ProfilesApp() {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Box(modifier = Modifier.padding(innerPadding)) {
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
             Text("Hello, ParcelHub!")
         }
     }
